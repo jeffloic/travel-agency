03d85adaaf19ddd3c18925b6ebd8f8453607c791
Loic <loicnondah@gmail.com>
2019-06-03 00:04:36 +0000

    Completed the On Scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
 import MobileMenu from './components/MobileMenu';
+import OnScroll from './components/OnScroll';
 
 var mobileMenu = new MobileMenu();
+
+new OnScroll($('.feature-item'), '85%');
+new OnScroll($('.testimonial'), '60%');
