12af7269e7c0e89f2efaf98f28273aa43366c92b
Loic <loicnondah@gmail.com>
2019-06-04 01:07:00 +0000

    Added browser support fallbacks

--- gulp/tasks/modernizr.js
+++ gulp/tasks/modernizr.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp'),
-webpack = require('webpack');
 modernizr = require('gulp-modernizr');
-
 
 gulp.task('modernizr', function() {
 	return gulp.src(['./app/assets/styles/**/*.css', './app/assets/scripts/**/*.js'])
@@ -12,11 +10,3 @@ gulp.task('modernizr', function() {
 		}))
 		.pipe(gulp.dest('./app/src/scripts/'));
 });
-
-gulp.task('scripts' , gulp.series('modernizr', function(callback) {
-	(webpack(require('../../webpack.config.js'), function() {
-		callback();
-	}))
-}));
-
-
